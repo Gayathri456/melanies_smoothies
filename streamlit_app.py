5f327923567b546801d04ac1bbcb9c7604e6b771
Gayathri456 <38349044+Gayathri456@users.noreply.github.com>
2024-07-18 13:03:49 +0530

    Update streamlit_app.py
    copied from my custom Smoothie SIS app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
@@ -10,15 +11,7 @@ st.write(
 )
 
 name_on_order = st.text_input("Name on Smoothie")
-st.write("The name on your Smoothie will be", name_on_order)
-
-#option = st.selectbox(
-#    "What is your favorite fruit ",
-#    ("Banana", "Strawberries", "Peaches"))
-
-#st.write("Your favorite fruit is:", option)
-
-from snowflake.snowpark.functions import col
+st.write("The name on your Smoothie will be: ", name_on_order)
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -30,24 +23,20 @@ ingredients_list =st.multiselect (
 )
 if ingredients_list:
    # st.write(ingredients_list)
-   # st.text(ingredients_list)
-
-    ingredients_string=''
-
+    #st.text(ingredients_list)
+    
+    ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
-        ingredients_string +=fruit_chosen + ' '
-    # st.write(ingredients_string)
-
-    
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-                values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
-    
+        ingredients_string += fruit_chosen + ' '
+    #st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+        
     #st.write(my_insert_stmt)
+    #st.stop()
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-       session.sql(my_insert_stmt).collect()
-       st.success('Your Smoothie is ordered!', icon="✅")
-   
-
-
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
